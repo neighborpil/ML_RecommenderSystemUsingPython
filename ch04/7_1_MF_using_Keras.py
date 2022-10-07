48d14d1fa4692c977f2329b748cc02606d2a9bc8
Neighborpil <neighborpil@naver.com>
2022-10-07 19:02:10 +0900

    save

--- ch04/7_1_MF_using_Keras.py
+++ ch04/7_1_MF_using_Keras.py
@@ -1,6 +1,39 @@
+import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Embedding, Dot, Add, Flatten
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import SGD, Adamax
+
+from sklearn.utils import shuffle
+
+# read csv
+r_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
+ratings = pd.read_csv('./u.data', names=r_cols, sep='\t', encoding='latin-1')
+ratings = ratings[['user_id', 'movie_id', 'rating']].astype(int)  # remove timestamp
+
+print(ratings.head())
+
+# split train test set
+TRAIN_SIZE = 0.75
+ratings = shuffle(ratings)
+cutoff = int(TRAIN_SIZE * len(ratings))
+ratings_train = ratings.iloc[:cutoff]
+ratings_test = ratings.iloc[cutoff:]
+
+
+# initialize variables
+K = 200
+mu = ratings_train.rating.mean()
+M = ratings.user_id.max() + 1 # dataset is ordered. be careful when I use it
+N = ratings.movie_id.max() + 1
+
+def RMSE(y_true, y_pred):
+    return tf.sqrt(tf.reduce_mean(tf.square(y_true - y_pred)))
+
+# keras model
+user = Input(shape=(1, ))
+item = Input(shape=(1, ))
+P_embedding = Embedding(M, K, embeddings_regularizer=l2())(user)
+Q_embedding = Embedding(N, K, embeddings_regularizer=l2())(item)
